707a16885c336b9618bf0ce3aee52f4103bbe4a5
diogods7 <diogosalgueiro16@gmail.com>
2023-08-04 15:22:41 -0300

    ooi

--- JS-Ex02/script.js
+++ JS-Ex02/script.js
@@ -1,19 +1,42 @@
-let student = []
-let joao = new student [{
+const student = [{
     nome: "João", 
     nota1: 7,
     nota2: 9 
+}, 
+{
+    nome: "Melissa", 
+    nota1: 5,
+    nota2: 8 
+},
+{
+    nome: "Vander", 
+    nota1: 5,
+    nota2: 10 
+},
+{
+    nome: "Jean", 
+    nota1: 6,
+    nota2: 9 
 }]
 
+
 function media() {
-    let i = 0;
-    let result;
-    while(student[i]=null){
+    let i=0
+    let result
+    while(student[i]!=null){
         result = (student[i].nota1 + student[i].nota2) / 2
+        result = result.toFixed(2)
+        let mensage
         if (result>7) {
-            return ""
-        } 
+            mensage = `A média do(a) aluno(a) ${student[i].nome} é: ${result}
+            Parabéns, ${student[i].nome}! Você foi aprovado(a) no concurso!`
+        } else {
+            mensage = `A média do(a) aluno(a) ${student[i].nome} é: ${result}
+            Não foi dessa vez, ${student[i].nome}! Tente Novamente!`
+        }
+        i++
+        alert(mensage) 
     }
 }
 
-alert(media)
+media()
