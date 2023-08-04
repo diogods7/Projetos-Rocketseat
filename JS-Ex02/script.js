0538f0f903d611112779d9a2399b05bd6832ebfd
diogods7 <diogosalgueiro16@gmail.com>
2023-08-04 11:36:22 -0300

    oi

---
+++ JS-Ex02/script.js
@@ -0,0 +1,15 @@
+let student = []
+let joao = new student [{
+    nome: "João", 
+    nota1: 7,
+    nota2: 9 
+}]
+
+function media() {
+    let i = 0
+    while(student[i]=null){
+        
+    }
+}
+
+alert(media)
